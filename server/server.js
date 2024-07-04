9facc61dbc1c854d12887b787d651f9cd2e980e4
Ankit Aditya <ankit.see@gmail.com>
2024-07-04 14:12:09 +0530

    quick fix

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 
-const connectDB = require('./config/db')
+const connectDB = require('../config/db')
 
 const app = express();
 
 connectDB()
 
 app.use(express.json({ extended: false }))
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/users', require('../routes/api/users'))
+app.use('/api/auth', require('../routes/api/auth'))
+app.use('/api/profile', require('../routes/api/profile'))
+app.use('/api/posts', require('../routes/api/posts'))
 
 app.use(express.static(path.join(__dirname, '../build')));
 
